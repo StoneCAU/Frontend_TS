5b9c8653c465378bae277c55ce4012f12998492d
sanghuniolsida <p0526000@gmail.com>
2025-08-25 10:54:01 +0900

    style: (usernotedetail-관련카드) refine card layout and text styles

--- src/components/UserNoteDetailcomponents/RelatedCard.tsx
+++ src/components/UserNoteDetailcomponents/RelatedCard.tsx
@@ -8,23 +8,33 @@ interface RelatedCardProps {
     authorHandle: string;
 }
 
-const RelatedCard: React.FC<RelatedCardProps> = ({ imageUrl, title, description, authorHandle }) => {
+const RelatedCard: React.FC<RelatedCardProps> = ({ title, description, authorHandle }) => {
     return (
         <div>
-            <h2 className="text-[19px] font-semibold text-white mb-4">이 유저노트와 같이 쓰였어요</h2>
-            <div className="flex flex-col space-x-4 bg-[#141924] p-3 rounded-lg">
-                <img src={aven} alt={title} className="w-[150px] h-[120px] object-cover rounded-[12px]" />
-                <div className="flex-grow">
-                    <h3 className="font-bold text-white">{title}</h3>
-                    <p className="text-sm text-gray-400 mt-1">{description}</p>
-                    <div className="mt-3">
-                        <span className="bg-gray-700 text-gray-300 text-xs font-medium px-2 py-1 rounded">
+            <h2 className="text-[19px] font-semibold text-white mb-4">
+                이 유저노트와 같이 쓰였어요
+            </h2>
+
+            <div className="flex flex-col space-y-3 bg-[#141924] p-3 rounded-lg">
+                <img
+                    src={aven}
+                    alt={title}
+                    className="w-[165px] h-[165px] object-cover rounded-[12px]"
+                />
+
+                <div className="flex-grow w-[165px]">
+                    <h3 className="font-normal text-white text-[15px]">{title}</h3>
+                    <p className="text-[12px] [color:#9CA3AF] mt-1">{description}</p>
+
+                    <div className="w-[60px] mt-3 bg-[#363A43] rounded-[6px] flex justify-center items-center">
+                        <span className="inline-block bg-gray-700 [color:#9CA3AF] text-[9px] font-medium px-2 py-1 rounded mb-[3px]">
                             {authorHandle}
                         </span>
                     </div>
                 </div>
             </div>
         </div>
+
     );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 // tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 export default {
+    important: true,
     content: [
         "./index.html",
         "./src/**/*.{js,ts,jsx,tsx}",
